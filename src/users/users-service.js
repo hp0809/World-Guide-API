8ef2b49b8f0be286e84f7adc1acd9eef75a47797
Hali Power <hpower0809@gmail.com>
2019-09-30 13:30:51 +0000

    update

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs');
 const xss = require('xss');
 
-const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
+//const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 
 const UsersService = {
     listUsers(db) {
@@ -33,9 +33,9 @@ const UsersService = {
       if (password.startsWith(' ') || password.endsWith(' ')) {
         return 'Password must not start or end with empty spaces'
       }
-      if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
-        return 'Password must contain one upper case, lower case, number and special character'
-      }
+      //if (!REGEX_UPPER_LOWER_NUMBER_SPECIAL.test(password)) {
+        //return 'Password must contain one upper case, lower case, number and special character'
+      //}
       return null
     },
     hashPassword(password) {
